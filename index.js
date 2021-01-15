8a4371aefae5916383008310c842147702dc67d0
TornikeJ <jmukhadze.tornike@hotmail.com>
2021-01-15 14:23:51 +0400

    changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var key;
 var keyCode;
 var count = 0
+var text;
 $(document).ready(function(){
     //Enter ღილაკით წერა
     $('input').keydown(function(e){
@@ -22,33 +23,27 @@ $(document).ready(function(){
           $('#div-show').show()
           $('#alert-show').hide()
         var text = $('#text-here').val();
-        data = $('<tr class="mt-1"><td class="col-md-1 border-one"><b>'+ ++count +'.'+'</b></td> <td class="col-md-3"><b>'+text +'</b></td><td class="col-md-1 border-two table-input"><input type="checkbox" name="record" id="check"></td></tr>');
-
+        data = $('<tr class="mt-1"><td class="col-md-11 border-one"><b>'+text +'</b></td><td class="col-md-1 border-two table-input"><input type="checkbox" name="record" id="check"></td></tr>');
         $('#show').append(data)
         //localStorage გასაღები 
         key     = Math.floor(Math.random() * 10000000)
-        keyCode =   localStorage.setItem(key,$('#text-here').val())
+        keyCode =   localStorage.setItem(key,text)
         $('#text-here').val('')
-      }
-      
-      
-        
+      }        
     })
     //მონიშნულის წაშლა
     $('#remove').click(function(){
         $('#show').find('input[name="record"]').each(function(){
             if($(this).is(':checked')){
                 $(this).parents("tr").remove();  
-               $(this).parents( localStorage.removeItem(key)).remove()
-                --count            
+              localStorage.removeItem(key,text)
+                --count          
             }
         })
     })
    $('#all-remove').click(function(){
         $('#show').find("tr").remove()
-        localStorage.clear(keyCode)
+        localStorage.clear()
         count = 0
    })
-
 })
-
